dfa5ab1859634ce7e712f1ad3521e438dc80b000
wujin <1735744134@qq.com>
2020-10-23 20:57:22 +0800

    第六次提交

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h2>预祝大家前程似锦</h2>
+    <h2>预祝大家前程似锦！</h2>
     <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=324237248,1675971079&fm=26&gp=0.jpg"/>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603448369683&di=2b57cbc2aefb46e98d289d30e681445a&imgtype=0&src=http%3A%2F%2F07imgmini.eastday.com%2Fmobile%2F20200928%2F20200928190700_9b19db8ba762bb449ac70a49ceae8e60_19.jpeg"/>
     </div>
