ac4a3815b496ae5b67cc0303f9871c4c4c77c2e5
wujin <1735744134@qq.com>
2020-10-24 08:51:52 +0800

    第八次提交

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <h2>预祝大家前程似锦！！！！！！！</h2>
+    <h2>预祝大家前程似锦！！！</h2>
+    <img src="https://guli-file-wujin.oss-cn-shanghai.aliyuncs.com/IMG_20180506_130627.jpg"/>
+    <img src="https://guli-file-wujin.oss-cn-shanghai.aliyuncs.com/IMG_20170502_180434.jpg"/>
     <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=324237248,1675971079&fm=26&gp=0.jpg"/>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603448369683&di=2b57cbc2aefb46e98d289d30e681445a&imgtype=0&src=http%3A%2F%2F07imgmini.eastday.com%2Fmobile%2F20200928%2F20200928190700_9b19db8ba762bb449ac70a49ceae8e60_19.jpeg"/>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603468126904&di=b3fb6eee0a5be304965dee43ebfe09b4&imgtype=0&src=http%3A%2F%2Fgss0.bdstatic.com%2F7LsWdDW5_xN3otebn9fN2DJv%2Fdoc%2Fpic%2Fitem%2F562c11dfa9ec8a1317accf02fe03918fa1ecc09a.jpg"/>
